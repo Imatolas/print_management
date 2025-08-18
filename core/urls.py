b23f3b3f1e00f108dafad5dab297c220efc404c5
Imatolas <gs.imato@gmail.com>
2025-08-18 15:35:40 -0300

    Add stub routes for reports and settings

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('relatorios/', views.relatorios, name='relatorios'),
+    path('configuracoes/', views.configuracoes, name='configuracoes'),
 ]
