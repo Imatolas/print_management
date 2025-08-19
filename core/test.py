ccb01bf4c8fa6c79692f35277b8648f5582d9052
Imatolas <gs.imato@gmail.com>
2025-08-19 00:25:40 -0300

    z

---
+++ core/test.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class SmokeTests(TestCase):
+    def test_homepage(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
