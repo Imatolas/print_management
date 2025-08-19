5398f13137a0e039f54668587fd4b661ffa72887
Imatolas <gs.imato@gmail.com>
2025-08-19 11:44:26 -0300

    Improve empty component dropdown messaging

--- core/forms.py
+++ core/forms.py
@@ -20,10 +20,35 @@ class ProductForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'rows':3})
         }
 
+class ComponentSelect(forms.Select):
+    """Select que oculta a opção em branco e desabilita mensagens."""
+
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option = super().create_option(name, value, label, selected, index, subindex, attrs)
+        if value == "":
+            option["attrs"]["hidden"] = True
+        if value == "__none__":
+            option["attrs"]["disabled"] = True
+        return option
+
+
 class BOMItemForm(forms.ModelForm):
+    component = forms.ModelChoiceField(
+        queryset=Component.objects.all(),
+        empty_label="",
+        widget=ComponentSelect,
+        required=False,
+    )
+
     class Meta:
         model = BOMItem
-        fields = ['component', 'quantity']
+        fields = ["component", "quantity"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        field = self.fields["component"]
+        if not field.queryset.exists():
+            field.choices = [("", ""), ("__none__", "Nenhum componente cadastrado")]
 
 BOMFormSet = inlineformset_factory(Product, BOMItem, form=BOMItemForm, extra=1, can_delete=True)
 
