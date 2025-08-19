92ff5b22add13270ae4537cb226cd89b8d65e700
Imatolas <gs.imato@gmail.com>
2025-08-19 10:26:04 -0300

    Style component select to match form design

--- core/test.py
+++ core/test.py
@@ -15,22 +15,31 @@ class ProductBOMTests(TestCase):
         BOMItem.objects.create(product=prod, component=comp, quantity=6)
         self.assertEqual(prod.total_cost, 60)
 
-    def test_product_creation_redirects_to_bom(self):
+    def test_create_product_with_components(self):
+        comp = Component.objects.create(code="C1", name="Comp", unit_cost=5)
         data = {
             "code": "P1",
             "name": "Prod",
             "description": "",
             "qty_on_hand": "0",
+            "bom_items-TOTAL_FORMS": "1",
+            "bom_items-INITIAL_FORMS": "0",
+            "bom_items-MIN_NUM_FORMS": "0",
+            "bom_items-MAX_NUM_FORMS": "1000",
+            "bom_items-0-id": "",
+            "bom_items-0-component": str(comp.pk),
+            "bom_items-0-quantity": "6",
         }
         response = self.client.post(reverse("produtos-new"), data)
+        self.assertRedirects(response, reverse("estoque-produtos"))
         prod = Product.objects.get(code="P1")
-        self.assertRedirects(response, reverse("produtos-bom", args=[prod.pk]))
+        self.assertEqual(prod.bom_items.count(), 1)
+        self.assertEqual(prod.total_cost, 30)
 
-
-    def test_add_component_via_view(self):
+    def test_edit_product_add_component(self):
         comp = Component.objects.create(code="C1", name="Comp", unit_cost=5)
         prod = Product.objects.create(code="P1", name="Prod")
-        url = reverse("produtos-bom", args=[prod.pk])
+        url = reverse("produtos-edit", args=[prod.pk])
         data = {
             "code": prod.code,
             "name": prod.name,
@@ -40,10 +49,12 @@ class ProductBOMTests(TestCase):
             "bom_items-INITIAL_FORMS": "0",
             "bom_items-MIN_NUM_FORMS": "0",
             "bom_items-MAX_NUM_FORMS": "1000",
+            "bom_items-0-id": "",
             "bom_items-0-component": str(comp.pk),
             "bom_items-0-quantity": "6",
         }
         response = self.client.post(url, data)
         self.assertRedirects(response, reverse("estoque-produtos"))
+        prod.refresh_from_db()
         self.assertEqual(prod.bom_items.count(), 1)
         self.assertEqual(prod.total_cost, 30)
